1acaa2d549227132f89147267901422cda214400
kopsisnet <kopsis.net@outlook.com>
2025-09-07 01:35:23 +0300

    settings.py güncellendi

--- subbase_app/settings.py
+++ subbase_app/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = 'django-insecure-woir4t@03%hzuc75i-lmzgen9$@o8fv39%l=@thi_maujaiga('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['subbase-app.onrender.com']
 
